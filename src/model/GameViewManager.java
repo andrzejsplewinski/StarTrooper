e34061f7926523bfe1ea8b068126edc784b80e6c
Rzesiasty <andrzej.splewinski@gmail.com>
2019-06-15 17:09:09 +0200

    Creating a moving background

--- src/model/GameViewManager.java
+++ src/model/GameViewManager.java
@@ -3,10 +3,12 @@ package model;
 import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GameViewManager {
@@ -23,11 +25,14 @@ public class GameViewManager {
 
     private boolean isLeftKeyPressed;
     private boolean isRightKeyPressed;
+    private boolean isUpKeyPressed;
+    private boolean isDownKeyPressed;
     private int angle;
     private AnimationTimer gameTimer;
 
-
-
+    private GridPane gridPane1;
+    private GridPane gridPane2;
+    private final static String BACKGROUND_IMAGE = "view/resources/darkPurple.png";
 
 
     public GameViewManager() {
@@ -50,6 +55,10 @@ public class GameViewManager {
                     isLeftKeyPressed = true;
                 } else if (event.getCode() == KeyCode.RIGHT) {
                     isRightKeyPressed = true;
+                } else if (event.getCode() == KeyCode.UP) {
+                    isUpKeyPressed = true;
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    isDownKeyPressed = true;
                 }
             }
         });
@@ -60,71 +69,120 @@ public class GameViewManager {
                     isLeftKeyPressed = false;
                 } else if (event.getCode() == KeyCode.RIGHT) {
                     isRightKeyPressed = false;
+                } else if (event.getCode() == KeyCode.UP) {
+                    isUpKeyPressed = false;
+                } else if (event.getCode() == KeyCode.DOWN) {
+                    isDownKeyPressed = false;
                 }
             }
         });
     }
 
-    public void createNewGame(Stage menuStage, SHIP shipChoosen){
+    public void createNewGame(Stage menuStage, SHIP shipChoosen) {
         this.menuStage = menuStage;
         this.menuStage.hide();
+        createBackground();
         createShip(shipChoosen);
         createGameLoop();
         gameStage.show();
     }
-    private void createShip(SHIP shipChoosen){
+
+    private void createShip(SHIP shipChoosen) {
         ship = new ImageView(shipChoosen.getUrlShip());
-        ship.setLayoutX(GAME_WIDTH/2 - 50);
+        ship.setLayoutX(GAME_WIDTH / 2 - 50);
         ship.setLayoutY(GAME_HEIGHT - 90);
         gamePane.getChildren().add(ship);
 
     }
 
-    private void createGameLoop(){
+    private void createGameLoop() {
         gameTimer = new AnimationTimer() {
             @Override
             public void handle(long now) {
+                moveBackground();
                 movesShip();
             }
         };
         gameTimer.start();
     }
 
-    private void movesShip(){
-        if(isLeftKeyPressed && !isRightKeyPressed){
-            if(angle > -30){
+    private void movesShip() {
+        if (isLeftKeyPressed && !isRightKeyPressed) {
+            if (angle > -30) {
                 angle -= 5;
             }
             ship.setRotate(angle);
-            if(ship.getLayoutX() > -10){
-                ship.setLayoutX(ship.getLayoutX() - 3);
+            if (ship.getLayoutX() > -10) {
+                ship.setLayoutX(ship.getLayoutX() - 5);
             }
         }
-        if (isRightKeyPressed && !isLeftKeyPressed){
-            if(angle < 30){
+        if (isRightKeyPressed && !isLeftKeyPressed) {
+            if (angle < 30) {
                 angle += 5;
             }
             ship.setRotate(angle);
-            if(ship.getLayoutX() < 498){
-                ship.setLayoutX(ship.getLayoutX()+3);
+            if (ship.getLayoutX() < 495) {
+                ship.setLayoutX(ship.getLayoutX() + 5);
             }
         }
-        if(!isLeftKeyPressed && !isRightKeyPressed){
-            if(angle < 0){
+        if (!isLeftKeyPressed && !isRightKeyPressed) {
+            if (angle < 0) {
                 angle += 5;
-            } else if (angle > 0){
-                angle -= 5;
-            }
-            ship.setRotate(angle);
-        }
-        if(isLeftKeyPressed && isRightKeyPressed){
-            if(angle < 0){
-                angle += 5;
-            } else if (angle > 0){
+            } else if (angle > 0) {
                 angle -= 5;
             }
             ship.setRotate(angle);
         }
+        if (isLeftKeyPressed && isRightKeyPressed) {
+            if (angle < 0) {
+                angle += 5;
+            } else if (angle > 0) {
+                angle -= 5;
+            }
+            ship.setRotate(angle);
+        }
+        if (isUpKeyPressed && !isDownKeyPressed) {
+            if (ship.getLayoutY() > 10) {
+                ship.setLayoutY(ship.getLayoutY() - 5);
+            }
+        }
+        if (isDownKeyPressed && !isUpKeyPressed) {
+            if (ship.getLayoutY() < 700) {
+                ship.setLayoutY(ship.getLayoutY() + 5);
+            }
+        }
+
     }
 
+    private void createBackground() {
+        gridPane1 = new GridPane();
+        gridPane2 = new GridPane();
+
+        for (int i = 0; i < 12; i++) {
+            ImageView backgroundImage1 = new ImageView(BACKGROUND_IMAGE);
+            ImageView backgroundImage2 = new ImageView(BACKGROUND_IMAGE);
+
+            //Sets the column,row indeces for the child when contained in a gridpane.
+
+            GridPane.setConstraints(backgroundImage1, i % 3, i / 3);
+            GridPane.setConstraints(backgroundImage2, i % 3, i / 3);
+            gridPane1.getChildren().add(backgroundImage1);
+            gridPane2.getChildren().add(backgroundImage2);
+        }
+        gridPane2.setLayoutY(-1024);
+        gamePane.getChildren().add(gridPane1);
+        gamePane.getChildren().add(gridPane2);
+    }
+
+    private void moveBackground() {
+        gridPane1.setLayoutY(gridPane1.getLayoutY() + 0.5);
+        gridPane2.setLayoutY(gridPane2.getLayoutY() + 0.5);
+
+        if (gridPane1.getLayoutY() >= 1024) {
+            gridPane1.setLayoutY(-1024);
+        }
+        if (gridPane2.getLayoutY() >= 1024) {
+            gridPane2.setLayoutY(-1024);
+        }
+    }
 }
